ed726f834e505c94b6b941ef4dc7b31a7f250e01
Spoorthylokraj <hlspoorthy@gmail.com>
2025-06-19 16:14:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,5 +39,5 @@ app.post("/send", async (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(Server running on port ${port});
+  console.log(`Server running on port ${port}`);
 });
